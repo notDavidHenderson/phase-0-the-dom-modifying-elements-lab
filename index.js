35e32cad4e11e3f08a0de0c55f1be378eeef5f8c
notDavidHenderson <sn2catt@gmail.com>
2021-02-16 23:44:36 +0000

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main"); //no longer has dom node main
+main.remove();
+
+// has new header var that points to h1#vic
+const newHeader = document.createElement("h1");
+newHeader.id="victory"; 
+newHeader.innerHTML="David is the champion";
